f11c06fd0280593e36d67b833ad1c348d540268c
Roel Standaert <roel.standaert@ucll.be>
2025-10-17 10:04:31 +0200

    Added permitAll for /error

--- 05-Registratie/src/main/java/be/ucll/backend2/config/SecurityConfig.java
+++ 05-Registratie/src/main/java/be/ucll/backend2/config/SecurityConfig.java
@@ -20,6 +20,7 @@ public class SecurityConfig {
                         authorizeRequests ->
                                 authorizeRequests
                                         .requestMatchers(HttpMethod.POST, "/api/v1/users").permitAll()
+                                        .requestMatchers("/error").permitAll()
                                         .anyRequest().authenticated()
                 )
                 .build();
